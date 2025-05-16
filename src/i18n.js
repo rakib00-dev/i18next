200691b85c40bd540409817696883e44afffbfcb
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-05-16 10:16:00 +0600

    added i18next file

---
+++ src/i18n.js
@@ -0,0 +1,33 @@
+import i18next from 'i18next';
+import I18nextBrowserLanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
+
+i18next
+  .use(I18nextBrowserLanguageDetector)
+  .use(initReactI18next)
+  .init({
+    debug: true,
+    lng: 'en',
+    resources: {
+      en: {
+        translation: {
+          greeting: 'Hello, Welcome',
+        },
+      },
+      fr: {
+        translation: {
+          greeting: 'Bonjour, Bienvenue',
+        },
+      },
+      hi: {
+        translation: {
+          greeting: 'नमस्ते, स्वागत है',
+        },
+      },
+      bn: {
+        translation: {
+          greeting: 'হ্যালো, স্বাগতম',
+        },
+      },
+    },
+  });
