1446c196b367951a17bf9ee501fa750d6a2e9a8c
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-05-18 19:09:13 +0600

    added description to i18next and rendered to the web

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,14 @@ import './App.css';
 function App() {
   const { t } = useTranslation();
 
+  const { line1, line2 } = t('description');
+
   return (
     <div className="container">
       <LanguageSelector />
       <h1>{t('greeting')}</h1>
+      <p>{line1}</p>
+      <span>{line2}</span>
     </div>
   );
 }
--- src/i18n.js
+++ src/i18n.js
@@ -9,25 +9,42 @@ i18next
   .init({
     debug: true,
     fallbackLng: 'en',
+    returnObjects: true,
     resources: {
       en: {
         translation: {
           greeting: 'Hello, Welcome',
+          description: {
+            line1: "You're watching RoadsideCoder YouTube Channel",
+            line2: 'This is an Internationalisation Tutorial',
+          },
         },
       },
-      // fr: {
-      //   translation: {
-      //     greeting: 'Bonjour, Bienvenue',
-      //   },
-      // },
+      fr: {
+        translation: {
+          greeting: 'Bonjour, Bienvenue',
+          description: {
+            line1: 'Vous regardez la chaîne YouTube RoadsideCoder',
+            line2: "Ceci est un tutoriel d'internationalisation",
+          },
+        },
+      },
       hi: {
         translation: {
           greeting: 'नमस्ते, स्वागत है',
+          description: {
+            line1: 'आप RoadsideCoder YouTube चैनल देख रहे हैं',
+            line2: 'यह एक अंतरराष्ट्रीयकरण ट्यूटोरियल है',
+          },
         },
       },
       bn: {
         translation: {
           greeting: 'হ্যালো, স্বাগতম',
+          description: {
+            line1: 'আপনি RoadsideCoder ইউটিউব চ্যানেল দেখছেন',
+            line2: 'এটি একটি আন্তর্জাতিককরণ টিউটোরিয়াল',
+          },
         },
       },
     },
